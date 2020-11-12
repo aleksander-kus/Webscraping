42df81dca0cf3b38a15ea534bbbd56f6aacbcd86
Aleksander <aleksander.kus.stud@pw.edu.pl>
2020-11-12 20:44:52 +0100

    added script class with init method plus necessary imports

--- runeterra.py
+++ runeterra.py
@@ -1,2 +1,12 @@
+from bs4 import BeautifulSoup
+import difflib
+import send_message
+import os
+
+class Runeterra():
+    def __init__(self):
+        self.path = os.getcwd() + '/'
+        self.open_files()   
+
 if __name__ == "__main__":
     pass
