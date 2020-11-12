38cfad1c76b2d0e47f7f650f908dad1a2af58d6f
Aleksander <aleksander.kus.stud@pw.edu.pl>
2020-11-12 15:49:42 +0100

    added class initialization and destruction

--- webscraper.py
+++ webscraper.py
@@ -6,6 +6,19 @@ from selenium import webdriver
 
 class Webscraper():
 
+    def startDriver(self):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.headless = True
+        self.driver = webdriver.Chrome(options=chrome_options)
+        
+    def __init__(self):
+        self.path = '/home/student/bin/'
+        self.startDriver()
+        self.readConfigFile()
+
+    def __del__(self):
+        self.driver.close()
+
 
 if __name__ == "__main__":
     pass
