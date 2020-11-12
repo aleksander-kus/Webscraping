fe0facc80c78cacd05d8373fb74232638597c68d
Aleksander <aleksander.kus.stud@pw.edu.pl>
2020-11-12 20:49:39 +0100

    added run function

--- runeterra.py
+++ runeterra.py
@@ -17,6 +17,17 @@ class Runeterra():
         self.path = os.getcwd() + '/'
         self.open_files()
 
+    def run(self):
+        old_titles = [line.rstrip('\n') for line in self.titles_file]
+        new_titles = self.get_new_titles_from_source()
+        titles = self.compare_old_and_new_titles(old_titles, new_titles)
+        if len(titles) == 0:
+            return
+        msg = self.compose_message(titles)
+        send_message.send_message(msg[0], msg[1])
+        self.update_titles(new_titles)
+
+
     def __del__(self):
         self.source.close()
         self.titles_file.close()
